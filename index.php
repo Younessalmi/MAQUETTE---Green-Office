47f4685a04937324b0e3eba25ab02ea7ab66befc
Younessalmi <younessalmi193@gmail.com>
2021-08-07 19:22:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Oswald">
+<title>GREEN Office</title>
+
+<style>
+    html{
+        background-image : url("./img/fond.png");
+    }
+    #el{
+        width : 684px;
+        margin:0 auto;
+
+    }
+    h3{
+        margin-left:15px;
+    }
+
+    p{
+        margin-left:10px; 
+        margin-right:10px;
+    }
+    ul{
+        margin-left:150px;
+    }
+
+    button
+
+    {
+        margin-right:20px;
+    }
+
+</style>
+<style>
+.container
+            {
+                width           :684px;
+                background-color:#b8cb21;
+                height          :123px  
+            }
+</style>
+</head>
+
+
+<body>
+
+
+<div style="background-color:white;height:670px;" id="el">
+        <div class="container">
+            <div>
+                <img src="./img/logo.png" alt="logo" style="float:left;">
+                        <nav style="margin-left:170px;">
+                                <u><li style="display:inline-block;margin-top:50px; margin-left:80px;font-family:Arial;"><a style="">LOCATION</a></u></li>
+                                <u><li style="display:inline-block; margin-left:20px;font-family:Arial;"><a style="">ABOUT US</a></u></li>
+                                <u><li style="display:inline-block; margin-left:20px;font-family:Arial;"><a style="">CONTACT</a></u></li>
+                        </nav>
+            </div>
+        </div>
+
+                                                                                   <!----><!---->
+
+           <div>
+                <h3 style="color:#b8cb21;font-family:Arial;"><a><img src="./img/rbi3a.png" style="width:13px;">WHO ARE YOU</h3></a>
+
+                <p style="font-family:Arial;font-size:14px;"><img style="width:200px;margin-left:15px;float:left;margin-right:10px;" src="./img/who.jpg">Raptim igitur properantes ut motus sui rumores celeritate nimia praevenirent, vigore corporum 
+                ac levitate confisi per flexuosas semitas ad summitates collium tardius evadebant.<br>
+                Et cum superatis difficultatibus arduis ad supercilia venissent fluvii Melanis alti et verticosi, qui pro muro tuetur
+                accolas circumfusus, augente nocte adulta terrorem quievere paulisper lucem opperientes. arbitrabantur enim nullo inpediente 
+                transgressi inopino adcursu adposita quaeque vastare, sed in cassum labores pertulere gravissimos.</p>    
+
+                                                                                                  <div style="float:right;">
+                                                                                                  <a href="#" alt="READ more"><button style="background-color:#b8cb21;color:white;border:0px;padding:10px;">READ more</button></a>
+                                                                                                  </div>
+           </div>
+
+                                                                                     <!----><!---->
+    
+             <h3 style="color:#b8cb21;font-family:Arial;"><img src="./img/rbi3a.png" style="width:13px;">WHAT WE DO</h3></i> 
+
+            <div>
+    
+                <p style="font-family:Arial;font-size:14px;"><img style="width:200px;margin-left:15px;float:left;margin-right:10px;" src="./img/what.jpg">
+                Raptim igitur properantes ut motus sui rumores celeritate nimia praevenirent, vigore corporum ac levitate
+                                             confisi per flexuosas semitas ad summitates collium tardius evadebant.<br>
+                
+                                             Et cum superatis difficultatibus arduis ad supercilia venissent fluvii Melanis alti et verticosi, qui 
+                                             pro muro tuetur accolas circumfusus,augente nocte adulta terrorem quievere paulisper lucem opperientes.
+                
+                                            Raptim igitur properantes ut motus sui rumores celeritate nimia praevenirent, vigore corporum ac levitate
+                                            confisi per flexuosas semitas ad summitates collium tardius evadebant.</p>
+
+                                            <ul style="font-family:Arial;font-size:14px;margin-left:10px;">
+                                                <li>Lorem ipsum dolor sit amet</li>
+                                                <li>Consectetur adipisicing elit</li>
+                                                <li>sed do eiusmod tempor incididunt</li>
+                                                <li>ut labore et dolore magna aliqua</li>
+                                                <li>sed do eiusmod tempor incididunt</li>
+                                                <li>ut labore et dolore magna aliqua</li>
+                                            </ul>
+                                                    <div style="float:right;">
+                                                      <a href="#" alt="READ more"><button style="background-color:#b8cb21;color:white;border:0px;padding:10px;">READ more</button></a>
+                                                    </div>
+                                                                                                       
+
+             </div>
+
+</div>
+
+                                                                                       <!----><!---->
+
+            <footer>
+
+                    <div style="text-align:center;font-family:Arial;font-size:13px;">
+                    <p style="text-align:centre;"><img style="width:70px;height:70px;" src="./img/footer.png"></p>
+                    Cet exercice de<a href="www.google.fr"> 3W Academy</a> est mis à disposition <a href="www.google.fr">pour l'usage personnel des étudiants, Pas de Rediffusion - Attribution -
+                    Pas d'Utilisation Commerciale - Pas de Modification - International.</a>
+                    Les autorisations au-delà du champ de cette licence peuvent être obtenues auprès de <a href="mailto:contact@3wa.fr">contact@3wa.fr.</a> Les maquettes ont été réalisées par Justine Muller.
+                    </div>
+
+            </footer>
+
+
+</body>
+</html>
